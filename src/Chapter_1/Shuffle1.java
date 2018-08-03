69f8d7b6b23a4b803352784aa8c959aa1439e425
andygaas <andyhaas@mail.ru>
2018-08-03 23:11:41 +0700

    create new class

---
+++ src/Chapter_1/Shuffle1.java
@@ -0,0 +1,25 @@
+package Chapter_1;
+
+public class Shuffle1 {
+    public static void main(String[] args) {    //1
+        int x = 3;                              //2
+
+        while (x > 0) {
+            if (x > 2) {
+                System.out.print("a");          //3
+
+                x = x - 1;                      //4
+                System.out.print("-");
+            }
+            if (x == 2) {
+                System.out.print("b c");
+                x = x - 1;
+            }
+            if (x == 1) {
+                System.out.print("-");
+                System.out.print("d");
+                x = x - 1;
+            }
+        }
+    }
+}
