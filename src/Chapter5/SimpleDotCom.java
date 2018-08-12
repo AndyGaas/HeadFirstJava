fa51ae848d220c61aa41ba2ca59c4abc671ca901
andygaas <andyhaas@mail.ru>
2018-08-12 23:36:16 +0700

    create new class

---
+++ src/Chapter5/SimpleDotCom.java
@@ -0,0 +1,26 @@
+package Chapter5;
+
+public class SimpleDotCom {
+    int [] locationCells;
+    int numOfHits = 0;
+
+    private String checkYourSelf(String userGuess) {
+        return null;
+    }
+
+    public void setLocationCells(int[] locationCells) {
+        this.locationCells = locationCells;
+    }
+
+    public void setNumOfHits(int numOfHits) {
+        this.numOfHits = numOfHits;
+    }
+
+    public int[] getLocationCells() {
+        return locationCells;
+    }
+
+    public int getNumOfHits() {
+        return numOfHits;
+    }
+}
---
+++ src/Chapter5/SimpleDotComDrive.java
@@ -0,0 +1,7 @@
+package Chapter5;
+
+public class SimpleDotComDrive {
+    public static void main(String[] args) {
+
+    }
+}
